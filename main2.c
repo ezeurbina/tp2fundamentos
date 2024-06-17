361bf0647d3199bb016ea98f047c92d8af53ec06
eze <curbina@fi.uba.ar>
2024-06-17 13:47:29 -0300

    insertar, borrar e imprimir

--- main2.c
+++ main2.c
@@ -6,17 +6,27 @@
 #include "funciones2.c"
 
 int main() {
+    p_lista lista=crearLista();
+
     lata lata1_data = {"Marca1", "Rojo", 355.0};
     lata lata2_data = {"Marca2", "Azul", 500.0};
+    lata lata3_data = {"Marca3", "Verde", 235.0};
+    lata lata4_data = {"Marca4", "Violeta", 400.0};
     p_lata lata1 = &lata1_data;
     p_lata lata2 = &lata2_data;
+    p_lata lata3 = &lata3_data;
+    p_lata lata4 = &lata4_data;
+    //imprimirNodo(crearNodo(lata1));
+    //imprimirNodo(crearNodo(lata2));
 
-    imprimirNodo(crearNodo(lata1));
+    insertarNodo(lista,crearNodo(lata1));
+    insertarNodo(lista,crearNodo(lata2));
+    insertarNodo(lista,crearNodo(lata3));
+    insertarNodo(lista,crearNodo(lata4));
 
-    imprimirNodo(crearNodo(lata2));
-    
-    
-
+    imprimirLista(lista);
+    eliminarNodo(lista,1);
+    imprimirLista(lista);
 
     return 0;
 }
