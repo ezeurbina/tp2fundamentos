f32739e8df87071d02280e15e0b14a031509997c
Ezequiel <curbina@fi.uba.ar>
2024-06-19 21:41:05 -0300

    arreglo unciones del menu

--- funciones.c
+++ funciones.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/* crea un nodo al que se le asigna memoria con malloc, si no puede asignarse
+esa memoria imprime "Error" y devuelve NULL*/
 p_nodo crearNodo(p_lata lata){
     p_nodo nodo=(p_nodo)malloc (sizeof(nodo));
     if (nodo==NULL){
         printf("%s\n","error");
+        return NULL;
     }
     nodo->elemento=lata;
     nodo->siguiente=NULL;
     return nodo;
 }
 
+/*imprime el nodo*/
 void imprimirNodo(p_nodo nodo){
     printf("%s\n",nodo->elemento->Marca);
     printf("%s\n",nodo->elemento->Color);
@@ -20,20 +23,29 @@ void imprimirNodo(p_nodo nodo){
     printf("\n");
 } 
 
+/*crea la lista inicializando la cantidad de nodos en 0 y le da ele elemento NULL a nodo
+si no puede asignarse la memoria imprime "ERROR" y devuelve NULL*/
 p_lista crearLista(){
     p_lista lista=(p_lista)malloc(sizeof(lista));
+    if(lista==NULL){
+        printf("ERROR");
+        return NULL;
+    }
     lista->nodo=NULL;
     lista->cantidad_nodos=0;
     return lista;
 
 }
+/*devuelve 1(TRUE) si la lista está vacia y 0(FALSE) si no lo está
+La lista está vacía cuando el nodo es null y la cantidad de los mismos es cero*/
 int estaVacia(p_lista lista){
     if((lista->cantidad_nodos==0)&&(lista->nodo==NULL)){
         return 1;
     }
     return 0;
 }
-
+/*Cuando la lista está vacia, insertar el primer nodo al principio pero, si no está vacía, inserta 
+el nodo al siguiente del anterior*/
 void insertarNodo(p_lista lista, p_nodo nodo){
     if(estaVacia(lista)){
         lista->nodo=nodo;
@@ -47,7 +59,7 @@ void insertarNodo(p_lista lista, p_nodo nodo){
     lista->cantidad_nodos++;
 
 }
-
+/*muestra la lista por pantalla*/
 void imprimirLista(p_lista lista){
     p_nodo aux=lista->nodo;
     while(aux!=NULL){
@@ -55,7 +67,7 @@ void imprimirLista(p_lista lista){
         aux=aux->siguiente;
     }
 }
-
+/*elimina un nodo según el indice que reciba, luego, se libera la memoria*/
 void eliminarNodo(p_lista lista,int indice){
     if(!estaVacia(lista)){
         p_nodo aux,anterior;
@@ -75,6 +87,50 @@ void eliminarNodo(p_lista lista,int indice){
 
 
 }
+void leerArchivo(p_lista lista) {
+    FILE *archivo = fopen("latas.txt", "r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo");
+    }else{
+        char Marca[20];
+        char Color[20];
+        float tamanio;
+        while (fscanf(archivo, "%s %s %f", Marca, Color, &tamanio) == 3) {
+            p_lata nueva_lata = (p_lata)malloc(sizeof(lata));
+            strcpy(nueva_lata->Marca, Marca);
+            strcpy(nueva_lata->Color, Color);
+            nueva_lata->tamanio = tamanio;
+            insertarNodo(lista, crearNodo(nueva_lata));
+        }
+        fclose(archivo);
+        }
+}
 
 
-    
+float totalColor(p_lista lista,char color[]){
+    p_nodo aux;
+    float suma=0;
+    aux=lista->nodo;
+    while(aux!=NULL){
+        if ((strcmp(aux->elemento->Color,color))==0){
+            suma+=aux->elemento->tamanio;
+        }
+        aux=aux->siguiente;
+    }
+    return suma;
+
+}
+
+int totalLatasMarca(p_lista lista, char marca[]){
+    p_nodo aux;
+    int suma=0;
+    aux=lista->nodo;
+    while(aux!=NULL){
+        if((strcmp(aux->elemento->Marca,marca))==0){/*strcmp devuelve 0 cuando las cadenas son iguales*/
+            suma++;
+        }
+        aux=aux->siguiente;
+    }
+    return suma;
+
+}
--- lectura.c
+++ lectura.c
@@ -5,61 +5,11 @@
 #include "estructuras.h"
 #include "funciones.c"
 
-void leerArchivo(p_lista lista) {
-    FILE *archivo = fopen("latas.txt", "r");
-    if (archivo == NULL) {
-        printf("Error al abrir el archivo");
-    }else{
-        char Marca[20];
-        char Color[20];
-        float tamanio;
-    
-        while (fscanf(archivo, "%s %s %f", Marca, Color, &tamanio) == 3) {
-            p_lata nueva_lata = (p_lata)malloc(sizeof(lata));
-            strcpy(nueva_lata->Marca, Marca);
-            strcpy(nueva_lata->Color, Color);
-            nueva_lata->tamanio = tamanio;
-            insertarNodo(lista, crearNodo(nueva_lata));
-        }
-        fclose(archivo);
-
-    }
-}
-
-float totalColor(p_lista lista){
-    p_nodo aux;
-    float suma;
-    aux=lista->nodo;
-    while(aux->siguiente!=NULL){
-        suma+=aux->elemento->tamanio;
-        aux=aux->siguiente;
-    }
-    suma+=aux->elemento->tamanio;
-    
-    return suma;
-}
-
-int totalLatasMarca(p_lista lista, char dato){
-    p_nodo aux;
-    int suma=0;
-    aux=lista->nodo;
-    while(aux->siguiente!=NULL){
-        if(strcmp(aux->elemento->Marca,&dato)){
-            suma++;
-        }
-        aux=aux->siguiente;
-    }
-    //if(strcmp(aux->elemento->Marca,dato)){
-      //  suma++;
-    //}
-    return suma;
-
-}
-
 
 int main(){
     p_lista lista=crearLista();
     char color[20];
+    char marcaBuscada[] = "NEGRONI";
     float cantidad_color;
 
 
@@ -67,11 +17,11 @@ int main(){
 
     imprimirLista(lista);
 
-    cantidad_color=totalColor(lista);
+    cantidad_color=totalColor(lista,"NEGRO");
     printf("Total Color:%f litros\n",cantidad_color);
 
 
-    //printf("%i\n",totalLatasMarca(lista,"POLACRIN"));
+    printf("%i\n",totalLatasMarca(lista,marcaBuscada));
 
     return 1;
 }
